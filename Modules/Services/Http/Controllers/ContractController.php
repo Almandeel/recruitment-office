77e478a5a7d7f7104675dd07661d42bd9f74649d
HajMo <haj-mo@protonmail.com>
2020-11-24 11:25:10 +0200

    Fix an issue on contracts

--- Modules/Services/Http/Controllers/ContractController.php
+++ Modules/Services/Http/Controllers/ContractController.php
@@ -33,31 +33,31 @@ class ContractController extends Controller
         $countries = Country::all();
         $professions = Profession::all();
         $offices = Office::all();
-        
+
         $contracts = Contract::where('status', '!=', Contract::STATUS_BAILED)->orderBy('created_at');
         $first_contract = Contract::first();
         $from_date = is_null($request->from_date) ? (is_null($first_contract) ? date('Y-m-d') : $first_contract->created_at->format('Y-m-d')) : $request->from_date;
         $to_date = is_null($request->to_date) ? date('Y-m-d') : $request->to_date;
         $status = !is_null($request->status) ? $request->status : 'initial';
-        
+
         $from_date_time = $from_date . ' 00:00:00';
         $to_date_time = $to_date . ' 23:59:59';
         $contracts = $contracts->whereBetween('created_at', [$from_date_time, $to_date_time]);
         // $contracts = $contracts->whereBetween('created_at', [Carbon::parse($from_date)->startOfDay(), Carbon::parse($to_date)->endOfDay()]);
         // $contracts = $contracts->whereBetween('created_at', [$from_date->startOfDay(), $to_date->endOfDay()]);
-        
+
         $country_id = !is_null($request->country_id) ? $request->country_id : 'all';
         if ($country_id != 'all') {
             $contracts = $contracts->where('country_id', $country_id);
         }
-        
+
         $profession_id = !is_null($request->profession_id) ? $request->profession_id : 'all';
         if ($profession_id != 'all') {
             $contracts = $contracts->where('profession_id', $profession_id);
         }
-        
+
         $contracts = $contracts->get();
-        
+
         $office_id = !is_null($request->office_id) ? $request->office_id : 'all';
         if ($office_id != 'all') {
             $contracts = $contracts->filter(function($contract) use($office_id){
@@ -70,17 +70,17 @@ class ContractController extends Controller
                 return $contract->cv->gender == array_search($gender, Cv::GENDERS);
             });
         }
-        
+
         if ($status != 'all') {
             $contracts = $contracts->filter(function($contract) use($status){
                 return $contract->checkStatus($status);
             });
         }
         // dd($contracts, $status);
-        
+
         return view('services::contracts.index', compact('contracts', 'from_date', 'to_date', 'status', 'gender', 'office_id', 'country_id', 'profession_id', 'countries', 'offices', 'professions'));
     }
-    
+
     /**
     * Show the form for creating a new resource.
     * @return Response
@@ -99,7 +99,7 @@ class ContractController extends Controller
         ['#', $title],
         ]
         ];
-        
+
         $countries = Country::all();
         $professions = Profession::all();
         $statuses = Contract::STATUSES;
@@ -128,7 +128,7 @@ class ContractController extends Controller
             $profession_id = $cv->profession_id;
             $cv_id = !is_null($request->cv_id) ? $request->cv_id : 'all';
         }
-        
+
         if ($view == 'initial') {
             $contract = Contract::find($request->contract_id);
             $title = is_null($contract) ? 'إنشاء عقد مبدئي' : 'عقد مبدئي';
@@ -148,17 +148,17 @@ class ContractController extends Controller
                 'heading' => 'عقد مبدئي',
                 ];
             }
-            
+
             $compact = is_null($contract) ? compact('crumbs', 'cvs', 'cv', 'layout', 'countries', 'professions', 'country_id', 'profession_id', 'statuses', 'title', 'contract') : compact('crumbs', 'title', 'contract', 'layout');
             return view('services::contracts.' . $view, $compact);
         }
-        
+
         $customers = Customer::all();
         $offices = Office::all();
         $marketers = Marketer::all();
         return view('services::contracts.' . $view, compact('crumbs', 'marketers', 'customers', 'cvs', 'cv', 'layout', 'countries', 'offices', 'professions', 'country_id', 'office_id', 'profession_id', 'statuses'));
     }
-    
+
     /**
     * Store a newly created resource in storage.
     * @param  Request  $request
@@ -170,8 +170,11 @@ class ContractController extends Controller
             $request->validate([
                 'customer_name' => 'required|string',
                 'customer_phones' => 'required|string',
-                'customer_id_number' => 'numeric',
-                'visa' => 'nullable|numeric',
+                'id_number' => 'numeric|unique:customers,id',
+                'visa' => 'nullable|numeric|max:100',
+            ], [], [
+                'id_number' => 'رقم الهوية',
+                'customer_id_number' => 'رقم الهوية'
             ]);
             $data = $request->only(['cv_id', 'visa']);
             $cv = Cv::findOrFail($request->cv_id);
@@ -179,7 +182,7 @@ class ContractController extends Controller
             $data['office_id'] = $cv->office_id;
             $data['profession_id'] = $cv->profession_id;
             $data['amount'] = 0;
-            
+
             if ($request->customer_id_number) {
                 $customer = Customer::where('id_number', $request->customer_id_number)->first();
             }
@@ -206,7 +209,7 @@ class ContractController extends Controller
             $request->validate([
                 'phones' => 'unique:customers',
                 'customer_id_number' => [Rule::unique('customers', 'id_number')],
-                'visa' => 'nullable|numeric',
+                'visa' => 'nullable|numeric|max:100',
                 'details' => 'nullable|string',
                 'cv_id' => 'required|numeric',
                 'profession_id' => 'required',
@@ -215,7 +218,12 @@ class ContractController extends Controller
                 'marketer_id' => 'nullable',
                 'marketing_ratio' => 'nullable|numeric',
                 'id_number'   => 'string|nullable| unique:customers',
+            ], [], [
+                'id_number' => 'رقم الهوية',
+                'customer_id_number' => 'رقم الهوية',
+                'visa' => 'رقم التأشيرة',
             ]);
+
             $data = $request->except(['_token', 'marketer_id']);
             if (!array_key_exists('status', $data)) {
                 $data['status'] = Contract::STATUS_WORKING;
@@ -227,7 +235,7 @@ class ContractController extends Controller
             if ($request->profession_id == 'all' || $request->profession_id != $cv->profession_id) {
                 $data['profession_id'] = $cv->profession_id;
             }
-            
+
             if (is_null($request->customer_id)) {
                 $customer_data = [];
                 if (!is_null($request->customer_name)) {
@@ -243,13 +251,13 @@ class ContractController extends Controller
                     }
                 }
             }
-            
+
             if ($request->marketer_id && $request->marketing_ratio) {
                 $marketer = Marketer::firstOrCreate(['name' => $request->marketer_id]);
                 $data['marketer_id'] = $marketer->id;
-                
+
                 $debt = $request->marketing_ratio;
-                
+
                 $marketer->update([
                 'debt' => ($marketer->debt +  $debt)
                 ]);
@@ -257,22 +265,22 @@ class ContractController extends Controller
                 $marketer = Marketer::firstOrCreate(['name' => $request->marketer_id]);
                 $data['marketer_id'] = $marketer->id;
             }
-            
+
             $data['user_id'] = auth()->user()->id;
-            
+
             $contract = Contract::create($data);
-            
-            
+
+
             // $cv->contracting($contract->id, $request->status);
-            
+
             if ($contract) {
                 $contract->attach();
             }
             return redirect()->route('contracts.show', $contract->id)->with('success', __('global.operation_success'));
         }
-        
+
     }
-    
+
     /**
     * Show the specified resource.
     * @param  Contract  $contract
@@ -283,7 +291,7 @@ class ContractController extends Controller
         // dd($contract->vouchers->count());
         return view('services::contracts.show', compact('contract'));
     }
-    
+
     /**
     * Show the form for editing the specified resource.
     * @param  Contract  $contract
@@ -328,11 +336,11 @@ class ContractController extends Controller
             $profession_id = $cv->profession_id;
             $cv_id = !is_null($request->cv_id) ? $request->cv_id : 'all';
         }
-        
-        
+
+
         return view('services::contracts.edit', compact('contract', 'marketers', 'customers', 'cvs', 'cv', 'countries', 'offices', 'professions', 'country_id', 'office_id', 'profession_id', 'statuses'));
     }
-    
+
     /**
     * Update the specified resource in storage.
     * @param  Request  $request
@@ -349,7 +357,7 @@ class ContractController extends Controller
         'country_id' => 'required',
         'amount' => 'required|numeric|min:0',
         ]);
-        
+
         $data = $request->except(['_token', '_method', 'marketer_id']);
         $cv = Cv::findOrFail($request->cv_id);
         if ($request->country_id == 'all' || $request->country_id != $cv->country_id) {
@@ -358,7 +366,7 @@ class ContractController extends Controller
         if ($request->profession_id == 'all' || $request->profession_id != $cv->profession_id) {
             $data['profession_id'] = $cv->profession_id;
         }
-        
+
         if (is_null($request->customer_id)) {
             $customer_data = [];
             if (!is_null($request->customer_name)) {
@@ -374,15 +382,15 @@ class ContractController extends Controller
                 }
             }
         }
-        
+
         if ($contract->cvs->count()) {
             if (!$contract->cvs->contains($request->cv_id)) {
                 foreach($contract->cvs->where('pivot.status', Contract::STATUS_WORKING) as $c){
                     $contract->cvs()->updateExistingPivot($c, array('status' => Contract::STATUS_CANCELED), false);
                     $c->update(['status' => Cv::STATUS_ACCEPTED]);
                 }
-                
-                
+
+
                 $cv->contracting($contract->id, $request->status);
             }else{
                 $contract_cv = $contract->cvs->where('pivot.cv_id', $request->cv_id)->last();
@@ -393,7 +401,7 @@ class ContractController extends Controller
         }else{
             $cv->contracting($contract->id, $request->status);
         }
-        
+
         if ($request->marketer_id) {
             $marketer = Marketer::firstOrCreate(['name' => $request->marketer_id]);
             $data['marketer_id'] = $marketer->id;
@@ -403,17 +411,17 @@ class ContractController extends Controller
                 'debt' => ($contract->marketer->debt -  $contract->marketing_ratio)
                 ]);
             }
-            
+
             $marketer->update([
             'debt' => ($marketer->debt +  $debt)
             ]);
         }
-        
+
         $contract->update($data);
-        
+
         return back()->with('success', __('global.operation_success'));
     }
-    
+
     /**
     * Remove the specified resource from storage.
     * @param  Contract  $contract
