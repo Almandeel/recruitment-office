39ea839123a89e955fc767c54546bef50c15d401
HajMo <haj-mo@protonmail.com>
2020-11-25 01:48:08 +0200

    Add tafweeds table

---
+++ database/migrations/2020_11_25_012049_create_tafweeds_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTafweedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tafweeds', function (Blueprint $table) {
+            $table->bigIncrements('id'); 
+            $table->date('contract_date'); 
+            $table->integer('contract_id'); 
+            $table->integer('customer_id'); 
+            $table->integer('visa');   
+            $table->string('phone')->length(50); 
+            $table->string('gender')->length(50);  
+            $table->text('addr')->nullable(); 
+            $table->float('salary');
+            $table->string('marketer')->length(200); 
+            $table->float('comm'); 
+            $table->string('identification_num'); 
+            $table->integer('country_id'); 
+            $table->string('office');  
+            $table->string('recruitment_cv_name');
+            $table->string('recruitment_cv_passport');
+            $table->string('injaz_num');
+            $table->float('injaz_cost');
+            $table->string('contract_num');
+            $table->text('notes')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tafweeds');
+    }
+}
